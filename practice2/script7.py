22496c829a2024ecb79927e153d05f420c82e8c0
maetatu22 <zbn.ex.zz@gmail.com>
2020-11-07 18:50:40 +0900

    input練習2

--- practice2/script7.py
+++ practice2/script7.py
@@ -1,4 +1,5 @@
 apple_price = 200
+money = 1000
 
 input_count = input("購入するりんごの個数を入力してください：")
 
@@ -8,3 +9,14 @@ total_price = apple_price * count
 
 print('購入するりんごの個数は' + str(count) + '個です')
 print('支払い金額は' + str(total_price) + '円です')
+
+if money > total_price:
+  print("りんごを"+input_count+"個買いました")
+  balance = money - total_price
+  print("残金は"+str(balance)+"円です")
+elif money == total_price:
+  print("りんごを"+input_count+"個書いました")
+  print("財布が空になりました")
+elif money < total_price:
+  print("お金が足りません")
+  print("りんごを買えませんでした")
