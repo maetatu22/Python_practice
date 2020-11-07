13998cda0be088506235fb5b330bfe4182cc17c7
maetatu22 <zbn.ex.zz@gmail.com>
2020-11-07 23:24:57 +0900

    商品購入プログラム2

--- practice3/script10.py
+++ practice3/script10.py
@@ -3,3 +3,11 @@ for item_name in items:
   print("---------------------------------------------")
   item_price = str(items[item_name])
   print(item_name + "は1個" + item_price + "円です")
+
+  input_count = input("購入する"+item_name+"の個数を入力してください：")
+
+  print("購入する"+ item_name + "の個数は" + input_count + "個です")
+
+  count = int(input_count)
+  total_price = items[item_name] * count
+  print("支払い金額は" + str(total_price) + "円です")
