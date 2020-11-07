ad905fae474e6304aaac3c1a34ea1ed3020b4070
maetatu22 <zbn.ex.zz@gmail.com>
2020-11-07 18:23:34 +0900

    elif練習

---
+++ practice2/script4.py
@@ -0,0 +1,9 @@
+money = 100
+apple_price = 100
+
+if money > apple_price:
+    print('りんごを買うことができます')
+elif money == apple_price:
+    print("りんごを買うことができますが所持金が0になります")
+else:
+    print('お金が足りません')
