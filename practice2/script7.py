cae96e4a0559914c6502eafc4e08e02a6d60643a
maetatu22 <zbn.ex.zz@gmail.com>
2020-11-07 18:40:14 +0900

    input練習

---
+++ practice2/script7.py
@@ -0,0 +1,10 @@
+apple_price = 200
+
+input_count = input("購入するりんごの個数を入力してください：")
+
+
+count = int(input_count)
+total_price = apple_price * count
+
+print('購入するりんごの個数は' + str(count) + '個です')
+print('支払い金額は' + str(total_price) + '円です')
