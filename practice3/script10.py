bcade1953847d5848e739659d4d45450b0ace3a6
maetatu22 <zbn.ex.zz@gmail.com>
2020-11-07 23:06:18 +0900

    商品購入プログラム

---
+++ practice3/script10.py
@@ -0,0 +1,5 @@
+items = {"apple": 100, "banana": 200, "orange": 400}
+for item_name in items:
+  print("---------------------------------------------")
+  item_price = str(items[item_name])
+  print(item_name + "は1個" + item_price + "円です")
