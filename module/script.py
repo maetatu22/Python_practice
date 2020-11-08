d40c8e28d6b9816965f697e2d8ff745303293e7c
maetatu22 <zbn.ex.zz@gmail.com>
2020-11-08 23:58:14 +0900

    import練習

--- module/script.py
+++ module/script.py
@@ -1,24 +1,5 @@
 
-def validate(hand):
-    if hand < 0 or hand > 2:
-        return False
-    return True
-
-def print_hand(hand, name='ゲスト'):
-    hands = ['グー', 'チョキ', 'パー']
-    print(name + 'は' + hands[hand] + 'を出しました')
-
-def judge(player, computer):
-    if player == computer:
-        return '引き分け'
-    elif player == 0 and computer == 1:
-        return '勝ち'
-    elif player == 1 and computer == 2:
-        return '勝ち'
-    elif player == 2 and computer == 0:
-        return '勝ち'
-    else:
-        return '負け'
+import utils
 
 
 
@@ -29,20 +10,20 @@ print('何を出しますか？（0: グー, 1: チョキ, 2: パー）')
 player_hand = int(input('数字で入力してください：'))
 
 
-if validate(player_hand):
+if utils.validate(player_hand):
     computer_hand = 1
 
     if player_name == '':
 
-        print_hand(player_hand)
+        utils.print_hand(player_hand)
     else:
         
-        print_hand(player_hand, player_name)
+        utils.print_hand(player_hand, player_name)
 
 
-    print_hand(computer_hand, 'コンピュータ')
+    utils.print_hand(computer_hand, 'コンピュータ')
    
-    result = judge(player_hand, computer_hand)
+    result = utils.judge(player_hand, computer_hand)
     print('結果は' + result + 'でした')
 else:
     print('正しい数値を入力してください')
