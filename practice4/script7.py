ba071d655271cdf88fb7d8b44dbdc355ec16df77
maetatu22 <zbn.ex.zz@gmail.com>
2020-11-08 23:47:58 +0900

    ジャンケンゲーム制作4

--- practice4/script7.py
+++ practice4/script7.py
@@ -7,6 +7,14 @@ def print_hand(hand, name="ゲスt"):
   hands = ["グー", "チョキ", "パー"]
   print(name + "は" + hands[hand] + "を出しました")
 
+def judge(player, computer):
+  if player == computer:
+    return "引き分け"
+  elif (player == 0 and computer == 1) or (player == 1 and computer == 2) or (player == 2 and computer == 0):
+    return "勝ち"
+  else:
+    return "負け"
+
 print("じゃんけんをはじめます")
 player_name = input("名前を入力してください：")
 print('何を出しますか？（0: グー, 1: チョキ, 2: パー）')
@@ -21,5 +29,8 @@ if validate(player_hand):
     print_hand(player_hand, player_name)
 
   print_hand(computer_hand, "コンピューター")
+
+  result = judge(player_hand, computer_hand)
+  print("結果は"+result+"でした")
 else:
   print("正しい数値を入力してください")
