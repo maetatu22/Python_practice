8419bb8e1ad39b8d44f8565061ce5d848135a365
maetatu22 <zbn.ex.zz@gmail.com>
2020-11-08 23:36:06 +0900

    じゃんけんゲーム制作３

--- practice4/script7.py
+++ practice4/script7.py
@@ -12,10 +12,14 @@ player_name = input("名前を入力してください：")
 print('何を出しますか？（0: グー, 1: チョキ, 2: パー）')
 player_hand = int(input('数字で入力してください：'))
 
-if validate(player_hand) == True:
+if validate(player_hand):
+  computer_hand = 1
+
   if player_name == "":
     print_hand(player_hand)
   else:
     print_hand(player_hand, player_name)
+
+  print_hand(computer_hand, "コンピューター")
 else:
   print("正しい数値を入力してください")
