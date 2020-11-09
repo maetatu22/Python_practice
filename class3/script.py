ac2618f42a9f1b40ec60f8ad1f17ec0d61e6ac5f
maetatu22 <zbn.ex.zz@gmail.com>
2020-11-09 23:20:00 +0900

    継承練習4

--- class3/script.py
+++ class3/script.py
@@ -1,11 +1,7 @@
-from food import Food 
+from food import Food
 from drink import Drink
 
-food1 = Food("サンドイッチ", 500)
-print(food1.info())
-
-drink1 = Drink("コーヒー", 300)
-print(drink1.info())
-
+food1 = Food('サンドイッチ', 500)
 food1.calorie = 330
-food1.calorie_info()
+
+print(food1.info())
