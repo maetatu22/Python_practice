b5805c6843bc25d65178f0ecce3c43ccfdd6e2de
maetatu22 <zbn.ex.zz@gmail.com>
2020-11-09 19:44:54 +0900

    インスタンス　練習2

--- class/script.py
+++ class/script.py
@@ -9,3 +9,10 @@ print(menu_item1.name)
 menu_item1.price = 500
 print(menu_item1.price)
 
+menu_item2 = MenuItem()
+
+menu_item2.name = "チョコケーキ"
+print(menu_item2.name)
+
+menu_item2.price = 400
+print(menu_item2.price)
