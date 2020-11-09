fe93de430733c190786d4c6fb1246b3dcdcb4cfa
maetatu22 <zbn.ex.zz@gmail.com>
2020-11-09 23:35:36 +0900

    継承練習8

--- class3/script.py
+++ class3/script.py
@@ -4,6 +4,5 @@ from drink import Drink
 food1 = Food('サンドイッチ', 500, 330)
 print(food1.info())
 
-drink1 = Drink("コーヒー", 300)
-drink1.amount = 180
+drink1 = Drink("コーヒー", 300, 180)
 print(drink1.info())
