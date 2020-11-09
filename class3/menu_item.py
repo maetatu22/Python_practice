76d588c9dcdd474cb985e70150fb3b680d4f08bb
maetatu22 <zbn.ex.zz@gmail.com>
2020-11-09 23:10:33 +0900

    継承練習2

--- class3/menu_item.py
+++ class3/menu_item.py
@@ -1,10 +1,10 @@
 class MenuItem:
   def __init__(self, name, price):
     self.name = name
-    self.name = price
+    self.price = price
 
   def info(self):
-    return self.name + ': ¥' + str (self.price)
+    return self.name + ': ¥' + str(self.price)
 
   def get_total_price(self, count):
     total_price = self.price * count
