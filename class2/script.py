9c9f329e34c89b4458e0fc8c6c6764010c045503
maetatu22 <zbn.ex.zz@gmail.com>
2020-11-09 20:57:45 +0900

    インスタンスメソッド戻り値

--- class2/script.py
+++ class2/script.py
@@ -1,16 +1,16 @@
 class MenuItem: 
   def info(self):
-    print(self.name + ": ¥" + str(self.price))
+    return self.name + ": ¥" + str(self.price)
 
 
 menu_item1 = MenuItem()
 menu_item1.name = "サンドイッチ"
 menu_item1.price = 500
 
-menu_item1.info()
+print(menu_item1.info())
 
 menu_item2 = MenuItem()
 menu_item2.name = 'チョコケーキ'
 menu_item2.price = 400
 
-menu_item2.info()
+print(menu_item2.info())
